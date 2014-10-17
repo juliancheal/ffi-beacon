740d8a6d6ec2ba7b914720f18a44e6a1baa29c4c
Julian Cheal <julian.cheal@gmail.com>
2014-10-17 09:32:51 -0400

    Removed print_usage method, as not needed

--- beacon.rb
+++ beacon.rb
@@ -4,21 +4,17 @@ module Beacon
   
   ffi_lib 'objc', '/System/Library/Frameworks/Foundation.framework/Foundation', '/System/Library/Frameworks/IOBluetooth.framework/IOBluetooth',
   'bin/ibeacon'
-  
-  attach_function :print_usage, [], :void
+
   attach_function :print_version, [], :void
   callback :completion_function, [:string, :int, :int, :int, :int], :int
   
   attach_function :startWithTimeInterval, [:double, :completion_function], :void
 end
 
-# Beacon.print_usage
 # Beacon.print_version
-# trap("INT") { puts "Shutting down."; exit}
+
 trap("INT") { exit! }
 
-
-# Beacon.startWithTimeInterval(1.1)
 callback = Proc.new do |uuid, major, minor, power, rssi|
   puts "uuid: #{uuid}, major: #{major}, minor: #{minor}, power: #{power}, rssi: #{rssi}"
   return 1
